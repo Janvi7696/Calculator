ceef1b48671da270bfff8a2e42023325568c7cda
Janvi7696 <58322341+Janvi7696@users.noreply.github.com>
2019-12-02 14:48:19 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -49,8 +49,7 @@
         });
     });
 
-    //Raise to power
-    function myFunction1(){
+    function evaluate(){
         if(calc.demo.value.includes("^")){
             var array = calc.demo.value.split("^");
             calc.demo.value=Math.pow(array[0],array[1])
@@ -58,7 +57,7 @@
             calc.demo.value= eval(calc.demo.value);
         }
     }
-     function myFunction2(){
+     function evaluation(){
         if(calc.demo_1.value.includes("^")){
             var array = calc.demo_1.value.split("^");
             calc.demo_1.value=Math.pow(array[0],array[1])
@@ -69,6 +68,19 @@
     //clear operation 
     function clear_btn(){
         calc.demo.value=0;
+    }
+    //clear the enteries 
+    function clear_entry(){
+        calc.demo.value=0;
+    }
+
+    //clear operation for demo_1 
+    function clear_input(){
+        calc.demo_1.value=0;
+    }
+    //clear the values
+    function clear_value(){
+        calc.demo_1.value=0;
     }
     //square operation
     function sqr(){
@@ -121,6 +133,9 @@
     //log operation 
     function mathLog(){
         calc.demo_1.value = Math.log10(calc.demo_1.value);
+    }
+    function raise_power(){
+        calc.demo_1.value = Math.pow(calc.demo_1.value);
     }
     function Exp(){
         calc.demo_1.value= Math.Exp(calc.demo_1.value);
@@ -182,12 +197,25 @@ function del(){
         calc.demo.value = calc.demo.value.substring(0, calc.demo.value.length-1);
     }
 }
-document.getElementById("evaluate").addEventListener("click", myFunction1);
-document.getElementById("evaluation").addEventListener("click", myFunction2);
-document.getElementById("clear").addEventListener("click", clear);
-document.getElementById("Clear").addEventListener("click", clear);
+
+function del_func(){
+if(calc.demo_1.value==""){
+        calc.demo_1.value= 0;
+    }else if(calc.demo_1.value==0){
+        calc.demo_1.value= 0;
+    }else if(calc.demo_1.value.substring(0, calc.demo_1.value.length - 1)==0){
+        calc.demo_1.value= 0;
+    }else{
+        calc.demo_1.value = calc.demo_1.value.substring(0, calc.demo_1.value.length-1);
+    }
+}
+
+document.getElementById("evaluate").addEventListener("click", evaluate);
+document.getElementById("evaluation").addEventListener("click", evaluation);
+document.getElementById("clear").addEventListener("click", clear_btn);
+document.getElementById("clear_entry").addEventListener("click", clear_entry);
 document.getElementById("root").addEventListener("click", squareRoot);
-document.getElementById("raise_power").addEventListener("click", square);     
+document.getElementById("raise_power").addEventListener("click",raise_power );     
 document.getElementById("square").addEventListener("click", square);       
 document.getElementById("dividex").addEventListener("click", fraction); 
 document.getElementById("sin").addEventListener("click", mathSin); 
@@ -195,18 +223,16 @@ document.getElementById("cos").addEventListener("click", mathCos );
 document.getElementById("tan").addEventListener("click", mathTan );
 document.getElementById("Exponential").addEventListener("click", Exp ); 
 document.getElementById("Modulus").addEventListener("click", Mod);
-
-
-
-
 document.getElementById("delete").addEventListener("click", del);
+document.getElementById("remove").addEventListener("click", del_func);
 document.getElementById("mc").addEventListener('click',function(){
     pushStack();
     addMemory();
 });
 
+
 //Memory Read operation
-var sel = document.getElementById("select");
+/*var sel = document.getElementById("select");
 function selectValue(){ 
     if(calc.demo.value==0){
 
@@ -217,4 +243,4 @@ function selectValue(){
 
     sel.value="";
 }
-sel.addEventListener("change", selectValue);
+sel.addEventListener("change", selectValue);*/
