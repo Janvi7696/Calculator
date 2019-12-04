9566687eb0decfc1cfe634beacc4839d507d82af
Janvi7696 <58322341+Janvi7696@users.noreply.github.com>
2019-12-04 20:25:15 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,4 +1,4 @@
-    var btn = document.querySelectorAll('.button');
+var btn = document.querySelectorAll('.button');
  calc.demo.value=0;
 
     Array.prototype.forEach.call(btn, function(btn) {
@@ -8,11 +8,10 @@
             var btnClicked = this.getAttribute('data-value');
             if(document.getElementById("demo").value.length>=1){
 
-                var splitVal = document.getElementById("demo").value.split("0");
-                //console.log(splitVal);
+                var splitVal = document.getElementById("demo").value.split(0);
             }
 
-            if(document.getElementById("demo").value==0){
+            if(document.getElementById("demo").value=="0"){
 
                 document.getElementById("demo").value += btnClicked;
 
@@ -23,49 +22,18 @@
         });
     });
 
-//For scientific calculator
-
- var btn_1 = document.querySelectorAll('.button_1');
- calc.demo_1.value=0;
-
-    Array.prototype.forEach.call(btn_1, function(btn_1) {
-        btn_1.addEventListener('click', function() {
-
- var btnClicked = this.getAttribute('data-value');
-            if(document.getElementById("demo_1").value.length>=1){
-
-                var splitVal = document.getElementById("demo_1").value.split("0");
-                //console.log(splitVal);
-            }
-
-            if(document.getElementById("demo_1").value==0){
-
-                document.getElementById("demo_1").value += btnClicked;
-
-            }else{
-
-                document.getElementById("demo_1").value += btnClicked;
-            }
-        });
-    });
-
-    function evaluate(){
-        if(calc.demo.value.includes("^")){
+function evaluate(){
+       if(calc.demo.value.includes("^")){
             var array = calc.demo.value.split("^");
             calc.demo.value=Math.pow(array[0],array[1])
         }else{
             calc.demo.value= eval(calc.demo.value);
         }
     }
-     function evaluation(){
-        if(calc.demo_1.value.includes("^")){
-            var array = calc.demo_1.value.split("^");
-            calc.demo_1.value=Math.pow(array[0],array[1])
-        }else{
-            calc.demo_1.value= eval(calc.demo_1.value);
-        }
-    }
-    //clear operation 
+
+
+
+
     function clear_btn(){
         calc.demo.value=0;
     }
@@ -73,50 +41,42 @@
     function clear_entry(){
         calc.demo.value=0;
     }
-
-    //clear operation for demo_1 
-    function clear_input(){
-        calc.demo_1.value=0;
-    }
-    //clear the values
-    function clear_value(){
-        calc.demo_1.value=0;
-    }
-    //square operation
-    function sqr(){
-        calc.demo.value = Math.pow(calc.demo.value,2);
-    }
     //addition operation
-    function addition(val) {
+    function addition(val,result) {
         result += val;
     }
     //subtraction operation
-    function subtraction(val) {
+    function subtraction(val,result) {
         result -= val;
     }
     //For division
-    function division(val) {
+    function division(val,result) {
         result /= val;
     }
     //For multiplication
-    function multiplication(val) {
+    function multiplication(val,result) {
         result *= val;
     }
-    //For 
-    function square(val) {
-        return val * val;
+    function square(){
+        calc.demo.value = Math.pow(calc.demo.value,2);
     }
 
-    function squareRoot(val) {
-        return Math.sqrt(val);
+    function mathSqrt(){
+    calc.demo.value = Math.sqrt(calc.demo.value);
     }
 
-    function percentage(val, res) {
+    function plusMinus(input) {
+    if(input.value.substring(0, 1) == "-")
+        input.value = input.value.substring(1, input.value.length)
+    else
+        input.value = "-" + input.value
+}
+  function percentage(val, res) {
         return res * val / 100;
     }
 
-    function fraction(val) {
-        return 1 / val;
+    function fraction(param) {
+        return 1 / param;
     }
     //sin operation
     function mathSin(){
@@ -131,21 +91,23 @@
         calc.demo_1.value = Math.tan(calc.demo_1.value);
     }
     //log operation 
-    function mathLog(){
-        calc.demo_1.value = Math.log10(calc.demo_1.value);
-    }
+    function ln(form) {
+    form.demo_1.value = Math.log(form.demo_1.value);
+}
     function raise_power(){
         calc.demo_1.value = Math.pow(calc.demo_1.value);
     }
     function Exp(){
-        calc.demo_1.value= Math.Exp(calc.demo_1.value);
+        calc.demo_1.value= Math.exp(calc.demo_1.value);
     }
-    function Mod(){
-        calc.demo_1.value=Math.Mod(calc.demo_1.value);
-    }
+    function Mod(x,y){
+        
+  return Math.mod(x, y) != 0
+}
 
-     //push values to the memory
-    var queue =[];
+
+
+var queue =[];
         function pushStack(){
 
         if(Number.isNaN(+calc.demo.value)){
@@ -162,7 +124,7 @@
 
 }
 //Memory create 
-function addMemory(){
+/*function addMemory(){
     if(Number.isNaN(+calc.demo.value)){
 
     }else{
@@ -182,7 +144,7 @@ function addMemory(){
         select.insertBefore(option,select.lastChild);
     }
 }
-
+*/
 
 //Delete operation 
 function del(){
@@ -198,49 +160,55 @@ function del(){
     }
 }
 
-function del_func(){
-if(calc.demo_1.value==""){
-        calc.demo_1.value= 0;
-    }else if(calc.demo_1.value==0){
-        calc.demo_1.value= 0;
-    }else if(calc.demo_1.value.substring(0, calc.demo_1.value.length - 1)==0){
-        calc.demo_1.value= 0;
-    }else{
-        calc.demo_1.value = calc.demo_1.value.substring(0, calc.demo_1.value.length-1);
-    }
-}
+document.getElementById("plus").addEventListener("click", addition);
+document.getElementById("minus").addEventListener("click", subtraction);
+document.getElementById("multiply").addEventListener("click", multiplication);
+document.getElementById("divide").addEventListener("click", division);
+document.getElementById("percentage").addEventListener("click", percentage);
+document.getElementById("equal").addEventListener("click", evaluate);
+document.getElementById("clear-entry").addEventListener("click", clear_btn);
+document.getElementById("clear").addEventListener("click", clear_entry);
+document.getElementById("sqrt").addEventListener("click", mathSqrt);
+document.getElementById("sqr").addEventListener("click", square);
+//document.getElementById("raise_power").addEventListener("click",raise_power );
+//document.getElementById("fraction").addEventListener("click", fraction);      
+//document.getElementById("plus-minus").addEventListener("click",plusMinus);                   
+//document.getElementById("sin").addEventListener("click", mathSin); 
+//document.getElementById("cos").addEventListener("click", mathCos );
+//document.getElementById("tan").addEventListener("click", mathTan );
+//document.getElementById("Exponential").addEventListener("click", Exp ); 
+//document.getElementById("Modulus").addEventListener("click", Mod);
+document.getElementById("clear-element").addEventListener("click", del);
 
-document.getElementById("evaluate").addEventListener("click", evaluate);
-document.getElementById("evaluation").addEventListener("click", evaluation);
-document.getElementById("clear").addEventListener("click", clear_btn);
-document.getElementById("clear_entry").addEventListener("click", clear_entry);
-document.getElementById("root").addEventListener("click", squareRoot);
-document.getElementById("raise_power").addEventListener("click",raise_power );     
-document.getElementById("square").addEventListener("click", square);       
-document.getElementById("dividex").addEventListener("click", fraction); 
-document.getElementById("sin").addEventListener("click", mathSin); 
-document.getElementById("cos").addEventListener("click", mathCos );
-document.getElementById("tan").addEventListener("click", mathTan );
-document.getElementById("Exponential").addEventListener("click", Exp ); 
-document.getElementById("Modulus").addEventListener("click", Mod);
-document.getElementById("delete").addEventListener("click", del);
-document.getElementById("remove").addEventListener("click", del_func);
-document.getElementById("mc").addEventListener('click',function(){
+//document.getElementById("mc").addEventListener('click',function(){
     pushStack();
-    addMemory();
-});
+    //addMemory();
+/*var selScien = document.getElementById("scientific_div");
+var selProg = document.getElementById("prog_div");
+var selStand = document.getElementById("stand_div");
+selProg.style.display="none";
+selStand.style.display="none";
 
 
-//Memory Read operation
-/*var sel = document.getElementById("select");
-function selectValue(){ 
-    if(calc.demo.value==0){
+function showScientific(){
+    selScien.style.display="block";
+    selProg.style.display="none";
+    selStand.style.display="none";
 
-        calc.demo.value = sel.value;
-    }else{
-        calc.demo.value += sel.value;
-    }
-
-    sel.value="";
 }
-sel.addEventListener("change", selectValue);*/
+
+
+function showProg(){
+    selScien.style.display="none";
+    selStand.style.display="none";
+    selProg.style.display="block";
+
+}
+
+
+function showStand(){
+    selScien.style.display="none";
+    selStand.style.display="block";
+    selProg.style.display="none";
+
+}*/
